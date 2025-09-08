d484e89f84228deca11bb654501e0b7b5d2f234d
GabbiePurch <purchi375@morrisville.edu>
2025-09-08 15:46:49 -0400

    Video01: StartUpdate
    Completed Video 1

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(0.1f, 0.0f, 0.0f);
+    }
+}
