c642d6cc1dbc9db2ae3398dbcad8203c57243d36
GabbiePurch <purchi375@morrisville.edu>
2025-09-10 15:23:44 -0400

    Video08: GetComponents
    Finished video 8, color-changing walls and  objects.

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,7 @@ public class ObjectHit : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
+        GetComponent<MeshRenderer>().material.color = Color.black;
         Debug.Log("Something Hit Me!");
     }
 }
