e5ff5d853c2f83d777102b34be0ee30c70fefb70
GabbiePurch <purchi375@morrisville.edu>
2025-09-10 15:12:40 -0400

    Video06: Methods    Finished Part1
    Completed Video 6 and Part 1 of the Lab.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,14 +5,30 @@ public class Mover : MonoBehaviour
     [SerializeField] float moveSpeed = 10f;
     void Start()
     {
-        
+
+        PrintInstructions();
+
     }
 
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome To The Game!");
+        Debug.Log("To Move Using Arrow Keys Or WASD");
+        Debug.Log("Don't Bump Into Objects!");
+    }
+
+    void MovePlayer()
+    {
+
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, yValue, zValue);
+
     }
 }
